efbb737242f287b65ec9a13a60df5c567318a985
Nico <lawani.gk@gmail.com>
2022-12-18 14:29:11 +0100

    add new files

---
+++ factor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+num = 77
+def prime(x):
+
+for x in range(2, num - 1, -1):
+    if x % x == 0 and num % x = 0:
+        y = num / x
+        if y % y == 0 and y % 1 == 0:
+            print(f"{num}={x}x{y}")
+        else:
+            continue
